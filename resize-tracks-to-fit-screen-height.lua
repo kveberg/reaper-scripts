e6a1dafc57f870d0ea971a12ce5176a7a664dc59
Torbjørn Kveberg <tkvebbs@gmail.com>
2023-03-03 13:48:54 +0100

    Update resize-tracks-to-fit-screen-height.lua

--- resize-tracks-to-fit-screen-height.lua
+++ resize-tracks-to-fit-screen-height.lua
@@ -10,7 +10,6 @@ local set_this_pad_to_your_liking = 0
 
 ------------------------------- CODE THAT DOES THE DEED. -------------------------------
 set_this_pad_to_your_liking = set_this_pad_to_your_liking + 250     -- add ~space of UI 
-local previous_selection = {}
 local n_tracks = reaper.CountTracks(0)
 local _, _, w, h = reaper.my_getViewport(0, 0, 0, 0, 0, 0, 0, 0, 1) -- Closest thing to documentation here: https://forum.cockos.com/showpost.php?p=1883879&postcount=4
 local track_size = (h - set_this_pad_to_your_liking) / n_tracks
@@ -18,13 +17,9 @@ local track
 
 for i=1, n_tracks, 1 do
   track = reaper.GetTrack(0, i-1)
-  if reaper.IsTrackSelected(track) then table.insert(previous_selection, 1, i) end
+  local previously_selected = reaper.IsTrackSelected(track)
+  --if reaper.IsTrackSelected(track) then table.insert(previous_selection, 1, i) end
   reaper.SetTrackSelected(track, true)
   reaper.SetMediaTrackInfo_Value(track, "I_HEIGHTOVERRIDE", track_size)
-  reaper.SetTrackSelected(track, false)
-end
-
-for i, v in ipairs(previous_selection) do
-  track = reaper.GetTrack(0, v-1)
-  reaper.SetTrackSelected(track, true)
+  if not previously_selected then reaper.SetTrackSelected(track, false) end
 end
