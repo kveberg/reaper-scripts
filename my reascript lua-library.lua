0f708847f6a3fb37ab4c2343d9be59349b360982
Torbjørn Kveberg <tkvebbs@gmail.com>
2023-03-05 15:55:55 +0100

    Update my reascript lua-library.lua

--- my reascript lua-library.lua
+++ my reascript lua-library.lua
@@ -1,6 +1,32 @@
 --------------------------------------------------
 -- Just a collection of functions I find useful --
+-- I did NOT necessarily write all these myself --
 --------------------------------------------------
+
+function rgbToHex(rgb)
+  local hexadecimal = '0X'
+
+  for key, value in pairs(rgb) do
+    local hex = ''
+
+    while(value > 0)do
+      local index = math.fmod(value, 16) + 1
+      value = math.floor(value / 16)
+      hex = string.sub('0123456789ABCDEF', index, index) .. hex      
+    end
+
+    if(string.len(hex) == 0)then
+      hex = '00'
+
+    elseif(string.len(hex) == 1)then
+      hex = '0' .. hex
+    end
+
+    hexadecimal = hexadecimal .. hex
+  end
+
+  return hexadecimal
+end
 
 local function log(msg, verbose)
   -- add booleans DEBUG and DEBUG_VERBOSE along with a \n at the start of the script to keep things tidy
